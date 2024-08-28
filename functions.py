38a51cc13e67e34a916d23ab48f81ca27e500793
STORM-MCCTC <donnadio_mark@student.mahoningctc.com>
2024-08-28 09:34:15 -0400

    added function.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+#functions are essentially a block of code that does nothing until it is called somewhere
+# DRY! Don't repeat yourself.
+# DRY! Don't repeat yourself.
+# DRY! Don't repeat yourself.
+#How many times have you given the same advice, or directions, or whatever over and over again?
+#With funtions. you would be able to write the code once and then cal it whatever someone needed it.
+
+#"def" is what notifies the code that a function is about to start
+#my_function is the name of the function below.
+#"()" the parenthesisis for the a parameters. the block below has none. ":" the colon lets the code know that the block of code that follows is part of the function.
+def my_function ():
+    #anything that is indented is part of the function. if it is not indented the program reads it as 
+    print("my function")
