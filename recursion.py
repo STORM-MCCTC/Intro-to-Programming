f353a091be4f2c4ce0f7e5e97a5dd358ee5602db
STORM-MCCTC <donnadio_mark@student.mahoningctc.com>
2024-09-04 08:59:34 -0400

    Recursion.py

---
+++ recursion.py
@@ -0,0 +1,13 @@
+#below is an example of recursion.
+
+def my_recursion():
+    choice = input("would you like to play a game: ")
+    choice = choice.lower()
+    if(choice == "y"):
+        my_recursion()
+    elif(choice == "n"):
+        print("Have a nice day")
+    else:
+        exit()
+        my_recursion()
+my_recursion()
