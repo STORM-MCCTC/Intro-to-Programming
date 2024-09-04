8bca5e82fb479720410baa700f9c039c06854aab
STORM-MCCTC <donnadio_mark@student.mahoningctc.com>
2024-09-04 10:05:09 -0400

    calq

---
+++ calq.py
@@ -0,0 +1,40 @@
+def start():
+    operation = input("Input Opertion (+, -, *, /, ^): ")
+    num1 = input("First Number: ")
+    num2 = input("Second Number: ")
+
+    if operation == ("+"):
+        addition(num1, num2)
+    elif operation == ("-"):
+        subtraction(num1, num2)
+    elif operation == ("*"):
+        multiplication(num1, num2)
+    elif operation == ("/"):
+        division(num1, num2)
+    elif operation == ("^"):
+        exponent(num1, num2)
+    else:
+        print("Error: Invaild Operator")
+        print("")
+        start()
+
+def addition(var1, var2):
+    anw = (float(var1) + float(var2))
+    print((var1) + "+" + (var2) + "=" + str(anw))
+def subtraction(var1, var2):
+    anw = (float(var1) - float(var2))
+    print((var1) + "-" + (var2) + "=" + str(anw))
+def multiplication(var1, var2):
+    anw = (float(var1) * float(var2))
+    print((var1) + "*" + (var2) + "=" + str(anw))
+def division(var1, var2):
+    anw = (float(var1) / float(var2))
+    print((var1) + "/" + (var2) + "=" + str(anw))
+def exponent(var1, var2):
+    anw = (float(var1) ** float(var2))
+    print((var1) + "^" + (var2) + "=" + str(anw))
+    
+loop = "true"
+while (loop == "true"):
+    start()
+    print("")
