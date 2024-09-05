30c38d02cf389f835b3d0084efe20a037d835167
STORM-MCCTC <donnadio_mark@student.mahoningctc.com>
2024-09-05 10:22:11 -0400

    ...

--- calq.py
+++ calq.py
@@ -1,8 +1,9 @@
 def start():
+    #takes user input
     operation = input("Input Opertion (+, -, *, /, ^): ")
     num1 = input("First Number: ")
     num2 = input("Second Number: ")
-
+    
     if operation == ("+"):
         addition(num1, num2)
     elif operation == ("-"):
@@ -17,7 +18,7 @@ def start():
         print("Error: Invaild Operator")
         print("")
         start()
-
+        
 def addition(var1, var2):
     anw = (float(var1) + float(var2))
     print((var1) + "+" + (var2) + "=" + str(anw))
@@ -28,12 +29,17 @@ def multiplication(var1, var2):
     anw = (float(var1) * float(var2))
     print((var1) + "*" + (var2) + "=" + str(anw))
 def division(var1, var2):
-    anw = (float(var1) / float(var2))
-    print((var1) + "/" + (var2) + "=" + str(anw))
+    if var1 == "0":
+        print("divide by zero error")
+    elif var2 == "0":
+        print("divide by zero error")
+    else:
+        anw = (float(var1) / float(var2))
+        print((var1) + "/" + (var2) + "=" + str(anw))
 def exponent(var1, var2):
     anw = (float(var1) ** float(var2))
     print((var1) + "^" + (var2) + "=" + str(anw))
-    
+
 loop = "true"
 while (loop == "true"):
     start()
